196b91dc15c5eed1b8142f6650e5a07f13f35087
Dan D. <daniel.mi.do@gmail.com>
2024-08-18 14:08:43 +0100

    Fixed item singleton and added extra rule

--- PuzzleGenerator/puzzle_generator.gd
+++ PuzzleGenerator/puzzle_generator.gd
@@ -6,18 +6,22 @@ func _init() -> void:
 	push_error("PuzzleGenerator._init was called. Class is expected to be used statically.")
 
 static func generate_random_rule(size : Vector2i, placed : Array[Item]) -> Rule:
-	var r : int = randi_range(0, 0)
-	match r:
-		0:
-			return IsInPositionRule.generate_valid_rule(size, placed)
-
-	# Shouldn't be possible. Match/case broken
-	assert(false)
-	return null
+	var rule : Rule
+	var max_attempts : int = 5
+	var attempts = 0
+	while not rule and attempts < max_attempts:
+		var r : int = randi_range(0, 1)
+		attempts += 1
+		match r:
+			0:
+				rule = IsInPositionRule.generate_valid_rule(size, placed)
+			1:
+				rule = IsNotTouchingRule.generate_valid_rule(size, placed)
+	return rule
 
 static func generate_puzzle(size : Vector2i) -> Array[Rule]:
 	var items_placed : Array[Item]
-	for i in range(10):
+	for i in range(20):
 		var random_item : Item = ItemLoader.items.pick_random()
 		var position : Vector2i = Vector2i(randi_range(0, size.x), randi_range(0, size.y))
 		var placement : Item.Placement = random_item.get_placement(position, Item.Rotation.NONE)
@@ -25,16 +29,22 @@ static func generate_puzzle(size : Vector2i) -> Array[Rule]:
 	
 	
 	var result_rules : Array[Rule]
-	while result_rules.size() < 2:
+	var max_attempts : int = 5
+	var attempts : int = 0
+	while result_rules.size() < 4 and attempts < max_attempts:
 		var new_rule : Rule = generate_random_rule(size, items_placed)
+		var clash : bool = false
 		for rule : Rule in result_rules:
 			if rule.is_identical_to(new_rule):
-				continue
-		result_rules.append(new_rule)
+				attempts += 1
+				clash = true
+		if not clash:
+			result_rules.append(new_rule)
+			attempts = 0
 	
 	print("PUZZLE GENERATED =========")
-	print("Size    :", size)
-	print("Items   :", items_placed)
+	print("Size    : ", size)
+	print("Items   : ", items_placed.map(func (i): return i.item_name))
 	print("Rules   :")
 	for rule : Rule in result_rules:
 		print("    ", rule.get_debug_request())
